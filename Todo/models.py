05f57134d2446b1d504603222dec5c1b345e3fde
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2024-02-22 10:51:38 +0330

    created todo model

--- Todo/models.py
+++ Todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo_model(models.Model):
+    title=models.CharField(max_length=50)
+    content=models.TextField()
+    priority=models.IntegerField()
+    is_done=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
