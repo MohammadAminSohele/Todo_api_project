709520739a67cf6bfa4981c5c246058ee032a599
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2024-02-23 13:00:17 +0330

    created task models

--- review_drf/models.py
+++ review_drf/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Task_model(models.Model):
+    title=models.CharField(max_length=20)
+    content=models.TextField()
+    priority=models.IntegerField()
+    description=models.TextField()
+    created=models.DateTimeField(auto_now_add=True)
+    update=models.DateTimeField(auto_now=True)
+    description=models.TextField()
+
+    def __str__(self):
+        return self.title
