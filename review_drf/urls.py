f1322ed90d20b75b4f7ae55f27bca467f737c6d2
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2024-02-23 13:30:19 +0330

    created task update

--- review_drf/urls.py
+++ review_drf/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('task-list/', views.taskList, name="task-list"),
     path('task-detail/<int:pk>/', views.taskDetail, name="task-Detail"),
     path('task-create/', views.taskCreate, name="task-Create"),
+     path('task-update/<int:pk>/', views.taskUpdate, name="task-update"),
 ]
--- review_drf/views.py
+++ review_drf/views.py
@@ -24,13 +24,22 @@ def taskDetail(request:Request,pk):
 
 @api_view(['GET','POST'])
 def taskCreate(request:Request):
-    if request.method=='GET':
-        tasks_query=Task_model.objects.order_by('priority').all()
-        tasks_serializer=Task_serializer(instance=tasks_query,many=True)
-        return Response(data=tasks_serializer.data,status=status.HTTP_200_OK)
     if request.method=='POST':
         task_deserializer=Task_serializer(data=request.data)
         if task_deserializer.is_valid():
             task_deserializer.save()
             return Response(data=task_deserializer.data,status=status.HTTP_201_CREATED)
+        return Response(data=None,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def taskUpdate(request:Request,pk):
+    try:
+        task_query=Task_model.objects.get(id=pk)
+    except Task_model.DoesNotExist:
+        return Response(data=None,status=status.HTTP_404_NOT_FOUND)
+    if request.method=='PUT':
+        edit_task=Task_serializer(instance=task_query,data=request.data)
+        if edit_task.is_valid():
+            edit_task.save()
+            return Response(data=edit_task.data,status=status.HTTP_202_ACCEPTED)
         return Response(data=None,status=status.HTTP_400_BAD_REQUEST)
