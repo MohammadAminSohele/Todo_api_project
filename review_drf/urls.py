52862fb22271e000af6f739859deb1bdad9719a8
MohammadAminSohaili <mohammadaminsohele@gmail.com>
2024-02-23 13:06:20 +0330

    created tasklist

--- review_drf/urls.py
+++ review_drf/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .import views
+
 urlpatterns = [
-    
+    path('task-list/', views.taskList, name="task-list"),
 ]
--- review_drf/views.py
+++ review_drf/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import Task_model
+from .serializer import Task_serializer
+
+
 # Create your views here.
+@api_view(['GET'])
+def taskList(request:Request):
+    tasks_query=Task_model.objects.order_by('priority').all()
+    tasks_serializer=Task_serializer(instance=tasks_query,many=True)
+    return Response(data=tasks_serializer.data,status=status.HTTP_200_OK)
